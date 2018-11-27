788a54235b92cedd28840a0f6724e20907ed93d4
HeyItzGinger <league@iMac-2.attlocal.net>
2018-11-26 20:00:47 -0800

    11/26/18

---
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -0,0 +1,54 @@
+package _00_JButtons_with_Lambdas;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class LambdaButtons implements ActionListener {
+	private JFrame window = new JFrame();
+	private JButton addNumbers = new JButton("ADD 2 Numbers");
+	private JButton randNumber = new JButton("RANDOM NUMBER");
+	private JButton tellAJoke = new JButton("TELL A JOKE");
+	
+	public LambdaButtons() {
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(new FlowLayout());
+		window.add(addNumbers);
+		window.add(randNumber);
+		window.add(tellAJoke);
+		
+		//1. Call the addActionListener methods for each button. Use lambdas
+		//   to define to functionality of the buttons.
+		addNumbers.addActionListener(e -> {
+			System.out.println("5 + 5 = " + (5+5));
+		});
+		randNumber.addActionListener(e -> {
+			Random r = new Random();
+			int x = r.nextInt(5);
+			System.out.println(x);
+		});
+		tellAJoke.addActionListener(e -> {
+			System.out.println("adjkf;ljsdkl");
+		});
+		
+		window.setVisible(true);
+		window.pack();
+	}
+	
+	public static void main(String[] args) {
+		new LambdaButtons();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
